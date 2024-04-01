05e21c73ac6a1e45e73362bd4d41e7513dca249e
Thomas Wiest <twg-github@wiestfamily.org>
2024-04-01 00:59:54 -0400

    Another attempt at fundir

--- ostree-zfs-base/files/fundir/mk-fundir.sh
+++ ostree-zfs-base/files/fundir/mk-fundir.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+/sysroot/usr/bin/lsattr -d /sysroot
+/sysroot/usr/bin/chattr -V -i /sysroot
 mkdir -p -v /sysroot/fun
+/sysroot/usr/bin/chattr -V +i /sysroot
+/sysroot/usr/bin/lsattr -d /sysroot
